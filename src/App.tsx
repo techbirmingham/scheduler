8363f29cf19439f7cfbe94bf3065782ebc8776ec
techbirmingham <deon@techbirmingham.com>
2025-05-29 19:54:38 -0500

    Create AuthGate component

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,41 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Layout } from './components/Layout';
-import { GridView } from './views/GridView';
-import { TimelineView } from './views/TimelineView';
-import { ListView } from './views/ListView';
-import { MapView } from './views/MapView';
-import { SpeakersView } from './views/SpeakersView';
-import { SettingsView } from './views/SettingsView';
-import { AuthGate } from './components/AuthGate'; // Import the auth overlay
+// src/App.tsx
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import netlifyIdentity from 'netlify-identity-widget'
 
-// start built-in modal UI you can open with netlifyIdentity.open()
-import netlifyIdentity from 'netlify-identity-widget';
+import { AuthGate } from './components/AuthGate'
+import { Layout } from './components/Layout'
+import { GridView } from './views/GridView'
+import { TimelineView } from './views/TimelineView'
+import { ListView } from './views/ListView'
+import { MapView } from './views/MapView'
+import { SpeakersView } from './views/SpeakersView'
+import { SettingsView } from './views/SettingsView'
 
-// Initialize Netlify Identity
+// Initialize Netlify Identity (point APIUrl at your site)
 netlifyIdentity.init({
-  APIUrl: 'https://tb-scheduler/.netlify/identity'
-});
-// end built-in modal UI
+  APIUrl: 'https://tb-scheduler.netlify.app/.netlify/identity'
+})
 
 function App() {
   return (
     <Router>
-      {/* Blocks access unless user is logged in */}
-      <AuthGate />
-
-      {/* Your app still loads underneath, but gets blocked by overlay */}
-      <Layout>
-        <Routes>
-          <Route path="/" element={<GridView />} />
-          <Route path="/timeline" element={<TimelineView />} />
-          <Route path="/list" element={<ListView />} />
-          <Route path="/map" element={<MapView />} />
-          <Route path="/speakers" element={<SpeakersView />} />
-          <Route path="/settings" element={<SettingsView />} />
-        </Routes>
-      </Layout>
+      {/* This overlay blocks everything until you log in */}
+      <AuthGate>
+        {/* Once authenticated, your normal layout & routes appear */}
+        <Layout>
+          <Routes>
+            <Route path="/"        element={<GridView     />} />
+            <Route path="/timeline" element={<TimelineView />} />
+            <Route path="/list"     element={<ListView     />} />
+            <Route path="/map"      element={<MapView      />} />
+            <Route path="/speakers" element={<SpeakersView/>} />
+            <Route path="/settings" element={<SettingsView />} />
+          </Routes>
+        </Layout>
+      </AuthGate>
     </Router>
-  );
+  )
 }
 
-export default App;
+export default App
