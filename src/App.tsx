19cb7125a4af3610948b5d1bbc518448f3cea328
techbirmingham <deon@techbirmingham.com>
2025-05-29 19:14:07 -0500

    Create AuthGate component

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { ListView } from './views/ListView';
 import { MapView } from './views/MapView';
 import { SpeakersView } from './views/SpeakersView';
 import { SettingsView } from './views/SettingsView';
-import { AuthGate } from './components/AuthGate'; // 👈 Import the auth overlay
+import { AuthGate } from './components/AuthGate'; // Import the auth overlay
 
 // start built-in modal UI you can open with netlifyIdentity.open()
 import netlifyIdentity from 'netlify-identity-widget';
@@ -21,10 +21,10 @@ netlifyIdentity.init({
 function App() {
   return (
     <Router>
-      {/* 👇 Blocks access unless user is logged in */}
+      {/* Blocks access unless user is logged in */}
       <AuthGate />
 
-      {/* 👇 Your app still loads underneath, but gets blocked by overlay */}
+      {/* Your app still loads underneath, but gets blocked by overlay */}
       <Layout>
         <Routes>
           <Route path="/" element={<GridView />} />
