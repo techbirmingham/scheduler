aeb0155c251b5af9bdeb624186adf28cf25e208a
techbirmingham <deon@techbirmingham.com>
2025-05-31 13:45:03 -0500

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { AuthGate }      from './components/AuthGate'
-import { Layout }        from './components/Layout'
+import { Layout }        from './components/Layout' 
 import { GridView }      from './views/GridView'
 import { TimelineView }  from './views/TimelineView'
 import { ListView }      from './views/ListView'
