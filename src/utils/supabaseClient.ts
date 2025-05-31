78f4d0104199cbaee6b69347854de56953177fda
techbirmingham <deon@techbirmingham.com>
2025-05-31 14:39:06 -0500

    Updated supabaseClient.ts

--- src/utils/supabaseClient.ts
+++ src/utils/supabaseClient.ts
@@ -1,18 +1,5 @@
 // src/utils/supabaseClient.ts
 
-console.log('⚡️ import.meta.env:', import.meta.env);
-
-console.log(
-  '⚡ MODE / DEV:',
-  import.meta.env.MODE,
-  import.meta.env.DEV
-)
-console.log(
-  '⚡ SUPABASE URL / ANON KEY:',
-  import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_ANON_KEY
-)
-
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl  = import.meta.env.VITE_SUPABASE_URL;
